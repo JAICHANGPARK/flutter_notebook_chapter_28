c2a76192a16b4af717729ccefb9e4b2560febfa8
Dreamwalker <aristojeff@gmail.com>
2023-01-19 16:44:44 +0900

    Create ep1401_main.dart

---
+++ lib/ep1401_popular_creators/ep1401_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class PopularCreatorsApp extends StatelessWidget {
+  const PopularCreatorsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
