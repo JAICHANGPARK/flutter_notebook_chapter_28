44a71c036a9ab705a7d583ea6da90cc0ef837ed9
Dreamwalker <aristojeff@gmail.com>
2023-01-19 16:57:10 +0900

    Update ep1401_main.dart

--- lib/ep1401_popular_creators/ep1401_main.dart
+++ lib/ep1401_popular_creators/ep1401_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'view/popular_creators_main_page.dart';
+
 void main() {
   runApp(const PopularCreatorsApp());
 }
@@ -10,7 +12,7 @@ class PopularCreatorsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ,
+      home: PopularCreatorsMainPage(),
     );
   }
 }
