3f9e22b680bcdca7d9b6eae4bdaa4223b17965db
Dreamwalker <aristojeff@gmail.com>
2023-01-19 16:56:56 +0900

    Create popular_creators_main_page.dart

---
+++ lib/ep1401_popular_creators/view/popular_creators_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PopularCreatorsMainPage extends StatefulWidget {
+  const PopularCreatorsMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<PopularCreatorsMainPage> createState() => _PopularCreatorsMainPageState();
+}
+
+class _PopularCreatorsMainPageState extends State<PopularCreatorsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
