83945aab58311bb4ed7562a62b62e4eaff24317d
Dreamwalker <aristojeff@gmail.com>
2023-01-22 11:42:11 +0900

    Update ep1404_main.dart

--- lib/ep1404_audiobook_app/ep1404_main.dart
+++ lib/ep1404_audiobook_app/ep1404_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1404_audiobook_app/view/audiobooks_main_page.dart';
 
 void main() {
   runApp(AudiobooksApp());
@@ -9,6 +10,8 @@ class AudiobooksApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: AudioBooksMainPage(),
+    );
   }
 }
