bb6dfb39c7c0137f8ed0b6b61eb85a34d16aeb77
Dreamwalker <aristojeff@gmail.com>
2023-01-22 11:40:15 +0900

    Create ep1404_main.dart

---
+++ lib/ep1404_audiobook_app/ep1404_main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class AudiobooksApp extends StatelessWidget {
+  const AudiobooksApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
