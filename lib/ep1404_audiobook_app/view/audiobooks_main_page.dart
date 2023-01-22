7c9b4503c9763d23e9244a219ecd140887310d7e
Dreamwalker <aristojeff@gmail.com>
2023-01-22 11:41:12 +0900

    Create audiobooks_main_page.dart

---
+++ lib/ep1404_audiobook_app/view/audiobooks_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AudioBooksMainPage extends StatefulWidget {
+  const AudioBooksMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<AudioBooksMainPage> createState() => _AudioBooksMainPageState();
+}
+
+class _AudioBooksMainPageState extends State<AudioBooksMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
