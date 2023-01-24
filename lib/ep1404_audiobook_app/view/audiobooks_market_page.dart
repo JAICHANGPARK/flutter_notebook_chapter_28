de4a6891d006e1c25a2dbceea70a3107aff61669
Dreamwalker <aristojeff@gmail.com>
2023-01-24 09:08:45 +0900

    Update audiobooks_market_page.dart

--- lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
+++ lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class AudioMarketPage extends StatefulWidget {
+  const AudioMarketPage({Key? key}) : super(key: key);
+
+  @override
+  State<AudioMarketPage> createState() => _AudioMarketPageState();
+}
+
+class _AudioMarketPageState extends State<AudioMarketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
