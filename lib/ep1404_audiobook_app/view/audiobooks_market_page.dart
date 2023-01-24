e593b75b5e79a4dc0341fd7c82b7a78ef04655ed
Dreamwalker <aristojeff@gmail.com>
2023-01-24 09:15:27 +0900

    Update audiobooks_market_page.dart

--- lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
+++ lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
@@ -10,10 +10,26 @@ class AudioMarketPage extends StatefulWidget {
 class _AudioMarketPageState extends State<AudioMarketPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-      ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 24, 16, 0),
+      child: Column(
+        children: [
+          Container(
+            height: 58,
+            color: Colors.blue,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container();
+              },
+            ),
+          ),
+          Expanded(
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(), itemBuilder: (context, index) {}))
+        ],
+      ),
     );
   }
 }
