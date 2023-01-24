50f04e67986fd8dfb709fb7f638f2d2d1e667561
Dreamwalker <aristojeff@gmail.com>
2023-01-24 09:02:18 +0900

    Update audiobooks_menu.dart

--- lib/ep1404_audiobook_app/domain/audiobooks_menu.dart
+++ lib/ep1404_audiobook_app/domain/audiobooks_menu.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AudiobooksMenu {
   IconData? iconData;
   String? title;
+
+  AudiobooksMenu({this.iconData, this.title = "?"});
 }
+
+List<AudiobooksMenu> audiobooksMenu = [
+  AudiobooksMenu(
+    iconData: Icons.home_filled,
+  ),
+  AudiobooksMenu(
+    iconData: Icons.apps,
+  ),
+  AudiobooksMenu(
+    iconData: Icons.bookmarks_outlined,
+  ),
+  AudiobooksMenu(
+    iconData: Icons.person_outlined,
+  ),
+];
