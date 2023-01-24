ad1805c2a14f9b2005434a42522ea5fef27d5fae
Dreamwalker <aristojeff@gmail.com>
2023-01-24 09:03:37 +0900

    Update audiobooks_menu.dart

--- lib/ep1404_audiobook_app/domain/audiobooks_menu.dart
+++ lib/ep1404_audiobook_app/domain/audiobooks_menu.dart
@@ -4,21 +4,18 @@ import 'package:flutter/material.dart';
 class AudiobooksMenu {
   IconData? iconData;
   String? title;
+  int? index;
 
-  AudiobooksMenu({this.iconData, this.title = "?"});
+  AudiobooksMenu({
+    this.iconData,
+    this.title = "?",
+    this.index,
+  });
 }
 
 List<AudiobooksMenu> audiobooksMenu = [
-  AudiobooksMenu(
-    iconData: Icons.home_filled,
-  ),
-  AudiobooksMenu(
-    iconData: Icons.apps,
-  ),
-  AudiobooksMenu(
-    iconData: Icons.bookmarks_outlined,
-  ),
-  AudiobooksMenu(
-    iconData: Icons.person_outlined,
-  ),
+  AudiobooksMenu(iconData: Icons.home_filled, index: 0),
+  AudiobooksMenu(iconData: Icons.apps, index: 1),
+  AudiobooksMenu(iconData: Icons.bookmarks_outlined, index: 2),
+  AudiobooksMenu(iconData: Icons.person_outlined, index: 3),
 ];
