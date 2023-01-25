6804df8a277d9cac02073abf2c630b09c361172e
Dreamwalker <aristojeff@gmail.com>
2023-01-25 17:12:50 +0900

    Update audiobooks_style.dart

--- lib/ep1404_audiobook_app/audiobooks_style.dart
+++ lib/ep1404_audiobook_app/audiobooks_style.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AudiobooksStyles {
   static const Color primaryBlack = Color(0xff2f2f2f);
   static const Color primaryGold = Color(0xffBFA054);
+  static const Color secondaryGold = Color(0xfff5efe1);
 }
