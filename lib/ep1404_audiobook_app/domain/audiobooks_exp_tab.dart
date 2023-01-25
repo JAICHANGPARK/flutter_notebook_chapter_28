30b4573a06fb0bddb3d24c96ec3f809b566c246c
Dreamwalker <aristojeff@gmail.com>
2023-01-25 17:06:54 +0900

    Update audiobooks_exp_tab.dart

--- lib/ep1404_audiobook_app/domain/audiobooks_exp_tab.dart
+++ lib/ep1404_audiobook_app/domain/audiobooks_exp_tab.dart
@@ -1,10 +1,13 @@
 class AudiobookExpTab {
+  String? tabIcon;
   String? title;
   int? index;
 
-  AudiobookExpTab({this.title, this.index});
+  AudiobookExpTab({this.tabIcon = "🔥", this.title, this.index});
 }
 
 List<AudiobookExpTab> audiobooksExploreTab = [
-
+  AudiobookExpTab(tabIcon: "🔥", title: "Trending"),
+  AudiobookExpTab(tabIcon: "❤", title: "Romance"),
+  AudiobookExpTab(tabIcon: "👶", title: "Children")
 ];
