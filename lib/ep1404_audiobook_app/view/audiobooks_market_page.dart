496c955ce1d338ec5132e2c9ea1fca3fc14009dc
Dreamwalker <aristojeff@gmail.com>
2023-01-25 17:07:48 +0900

    Update audiobooks_market_page.dart

--- lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
+++ lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1404_audiobook_app/audiobooks_style.dart';
+import 'package:flutter_notebook_chapter_28/ep1404_audiobook_app/domain/audiobooks_exp_tab.dart';
 
 class AudioMarketPage extends StatefulWidget {
   const AudioMarketPage({Key? key}) : super(key: key);
@@ -19,21 +21,25 @@ class _AudioMarketPageState extends State<AudioMarketPage> {
             color: Colors.blue,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 10,
+              itemCount: audiobooksExploreTab.length,
               itemBuilder: (context, index) {
-                return Container();
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: primaryGold,
+                    ),
+                  ),
+                );
               },
             ),
           ),
-          SizedBox(height: 16,),
+          SizedBox(
+            height: 16,
+          ),
           Expanded(
             child: GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 16,
-                crossAxisSpacing: 16,
-                childAspectRatio: 8/12
-              ),
+                  crossAxisCount: 2, mainAxisSpacing: 16, crossAxisSpacing: 16, childAspectRatio: 8 / 12),
               itemCount: 100,
               itemBuilder: (context, index) {
                 return Container(
