3dd0f990a351135f40d12a83f3e7bacaa851d29c
Dreamwalker <aristojeff@gmail.com>
2023-01-26 17:46:41 +0900

    Update audiobooks_detail_page.dart

--- lib/ep1404_audiobook_app/view/audiobooks_detail_page.dart
+++ lib/ep1404_audiobook_app/view/audiobooks_detail_page.dart
@@ -187,13 +187,13 @@ class _AudiobooksDetailPageState extends State<AudiobooksDetailPage> {
                               text: "Synopsis",
                             ),
                             Tab(
-                              text: "Synopsis",
+                              text: "Details",
                             ),
                             Tab(
-                              text: "Synopsis",
+                              text: "Author",
                             ),
                             Tab(
-                              text: "Synopsis",
+                              text: "Reviews",
                             ),
                           ],
                         ))
@@ -203,7 +203,12 @@ class _AudiobooksDetailPageState extends State<AudiobooksDetailPage> {
                   Expanded(
                       child: TabBarView(
                     children: [
-                      Container(),
+                      SingleChildScrollView(
+                        child: Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                      ),
                       Container(),
                       Container(),
                       Container(),
