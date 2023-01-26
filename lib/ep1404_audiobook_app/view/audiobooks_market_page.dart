160c19e7341cd5e0c89a0e773830811389bfd658
Dreamwalker <aristojeff@gmail.com>
2023-01-26 17:26:01 +0900

    Update audiobooks_market_page.dart

--- lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
+++ lib/ep1404_audiobook_app/view/audiobooks_market_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_28/ep1404_audiobook_app/audiobooks_style.dart';
 import 'package:flutter_notebook_chapter_28/ep1404_audiobook_app/domain/audiobooks_exp_tab.dart';
+import 'package:flutter_notebook_chapter_28/ep1404_audiobook_app/view/audiobooks_detail_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AudioMarketPage extends StatefulWidget {
@@ -67,79 +68,87 @@ class _AudioMarketPageState extends State<AudioMarketPage> {
                     mainAxisSpacing: 20,
                     crossAxisSpacing: 20,
                     childAspectRatio: 7 / 12,
-
                   ),
                   itemCount: 100,
                   itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: AudiobooksStyles.secondaryGold,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.orange,
-                                  borderRadius: BorderRadius.circular(8),
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                        "https://cdn.pixabay.com/photo/2019/02/14/07/28/painting-3995999_960_720.jpg"),
-                                    fit: BoxFit.cover,
-                                  )),
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const AudiobooksDetailPage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AudiobooksStyles.secondaryGold,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.orange,
+                                    borderRadius: BorderRadius.circular(8),
+                                    image: const DecorationImage(
+                                      image: NetworkImage(
+                                          "https://cdn.pixabay.com/photo/2019/02/14/07/28/painting-3995999_960_720.jpg"),
+                                      fit: BoxFit.cover,
+                                    )),
+                              ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 8,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "The Replica",
-                                style: GoogleFonts.dmSerifDisplay(
-                                  fontSize: 16,
-                                ),
-                              ),
-                              Text(
-                                "By Dreamwalker",
-                                style: GoogleFonts.dmSerifDisplay(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 12,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "\$285",
-                                    style: GoogleFonts.dmSerifDisplay(fontSize: 16),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "The Replica",
+                                  style: GoogleFonts.dmSerifDisplay(
+                                    fontSize: 16,
                                   ),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.black,
-                                      borderRadius: BorderRadius.circular(4),
+                                ),
+                                Text(
+                                  "By Dreamwalker",
+                                  style: GoogleFonts.dmSerifDisplay(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 12,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "\$285",
+                                      style: GoogleFonts.dmSerifDisplay(fontSize: 16),
                                     ),
-                                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
-                                    child: const Center(
-                                      child: Text(
-                                        "Buy",
-                                        style: TextStyle(
-                                          color: Colors.white,
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius: BorderRadius.circular(4),
+                                      ),
+                                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+                                      child: const Center(
+                                        child: Text(
+                                          "Buy",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
-                          )
-                        ],
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
