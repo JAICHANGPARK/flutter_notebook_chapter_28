a46cf850ed63a440867831ff3fb440e4a0e346a7
Dreamwalker <aristojeff@gmail.com>
2023-01-27 18:12:13 +0900

    Update ep1409_main.dart

--- lib/ep1409_sound_music_streaming_app/ep1409_main.dart
+++ lib/ep1409_sound_music_streaming_app/ep1409_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1409_sound_music_streaming_app/login/view/sound_music_player_enter_page.dart';
 
 void main() {
   runApp(const SoundMusicStreamingApp());
@@ -9,8 +10,11 @@ class SoundMusicStreamingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-
+    return MaterialApp(
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => SoundMusicPlayerLoginPage(),
+      },
     );
   }
 }
