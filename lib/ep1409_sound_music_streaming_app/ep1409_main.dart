3d18a0639f057e54c52fd2f44be34a9fe790d27c
Dreamwalker <aristojeff@gmail.com>
2023-01-27 18:08:55 +0900

    Update ep1409_main.dart

--- lib/ep1409_sound_music_streaming_app/ep1409_main.dart
+++ lib/ep1409_sound_music_streaming_app/ep1409_main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const SoundMusicStreamingApp());
+}
 
 class SoundMusicStreamingApp extends StatelessWidget {
   const SoundMusicStreamingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp();
+    return const MaterialApp(
+      
+    );
   }
 }
