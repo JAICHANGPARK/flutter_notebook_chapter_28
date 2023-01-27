d17bb5289926020030c2fb66fb5aba8beba385c7
Dreamwalker <aristojeff@gmail.com>
2023-01-27 18:13:02 +0900

    Create smsa_main_page.dart

---
+++ lib/ep1409_sound_music_streaming_app/home/view/smsa_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// SMSA : Sound Music Streaming App 
+class SMSAMainPage extends StatefulWidget {
+  const SMSAMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<SMSAMainPage> createState() => _SMSAMainPageState();
+}
+
+class _SMSAMainPageState extends State<SMSAMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
