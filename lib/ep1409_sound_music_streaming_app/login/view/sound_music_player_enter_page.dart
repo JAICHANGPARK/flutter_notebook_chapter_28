9548ad3922a90e751b4314872230c7a9d917ef93
Dreamwalker <aristojeff@gmail.com>
2023-01-27 18:12:07 +0900

    Create sound_music_player_enter_page.dart

---
+++ lib/ep1409_sound_music_streaming_app/login/view/sound_music_player_enter_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SoundMusicPlayerLoginPage extends StatefulWidget {
+  const SoundMusicPlayerLoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<SoundMusicPlayerLoginPage> createState() => _SoundMusicPlayerLoginPageState();
+}
+
+class _SoundMusicPlayerLoginPageState extends State<SoundMusicPlayerLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
