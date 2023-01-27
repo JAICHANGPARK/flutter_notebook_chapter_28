bd4a293c46f389d2de841e4f15a9d81475288e7a
Dreamwalker <aristojeff@gmail.com>
2023-01-27 18:17:37 +0900

    Update sound_music_player_enter_page.dart

--- lib/ep1409_sound_music_streaming_app/login/view/sound_music_player_enter_page.dart
+++ lib/ep1409_sound_music_streaming_app/login/view/sound_music_player_enter_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SoundMusicPlayerLoginPage extends StatefulWidget {
   const SoundMusicPlayerLoginPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,48 @@ class SoundMusicPlayerLoginPage extends StatefulWidget {
 class _SoundMusicPlayerLoginPageState extends State<SoundMusicPlayerLoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage("https://cdn.pixabay.com/photo/2016/09/10/11/11/musician-1658887_960_720.jpg"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 72,
+            child: Column(
+              children: [
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "S",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.indigo,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "oundplay",
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
