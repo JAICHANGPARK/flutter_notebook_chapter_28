77a402e73b4ac4c60c39e64ef8c1bed9a24b4561
Dreamwalker <aristojeff@gmail.com>
2023-01-29 09:23:14 +0900

    Create smsa_menu_data.dart

---
+++ lib/ep1409_sound_music_streaming_app/home/domain/smsa_menu_data.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1409_sound_music_streaming_app/home/model/smsa_menu.dart';
+
+List<SmsaMenu> smsaMenuData = [
+  SmsaMenu(Icons.home_filled),
+  SmsaMenu(Icons.search),
+  SmsaMenu(Icons.favorite_border),
+  SmsaMenu(Icons.search),
+
+];
