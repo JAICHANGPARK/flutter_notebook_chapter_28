0dfdb445675ef5d0310bbcf5764061ec484aed51
Dreamwalker <aristojeff@gmail.com>
2023-01-29 09:26:58 +0900

    Create smsa_tab_menu_data.dart

---
+++ lib/ep1409_sound_music_streaming_app/home/domain/smsa_tab_menu_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_notebook_chapter_28/ep1409_sound_music_streaming_app/home/model/smsa_tab_menu.dart';
+
+List<SmsaTabMenu> smsaTabMenuData = [
+  SmsaTabMenu("Music"),
+  SmsaTabMenu("Podcast"),
+  SmsaTabMenu("Event"),
+  SmsaTabMenu("Artist"),
+];
