385c016f5fd92673f01e9134e5d15e2d8b64529d
Dreamwalker <aristojeff@gmail.com>
2023-01-29 09:35:23 +0900

    Update smsa_main_page.dart

--- lib/ep1409_sound_music_streaming_app/home/view/smsa_main_page.dart
+++ lib/ep1409_sound_music_streaming_app/home/view/smsa_main_page.dart
@@ -19,135 +19,156 @@ class _SMSAMainPageState extends State<SMSAMainPage> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Stack(
+        child: IndexedStack(
+          index: _menuIndex,
           children: [
-            Positioned.fill(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Good Morning\nHave a nice day!",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                          textAlign: TextAlign.start,
+            Stack(
+              children: [
+                Positioned.fill(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "Good Morning\nHave a nice day!",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                              textAlign: TextAlign.start,
+                            ),
+                            CircleAvatar(
+                              backgroundColor: Colors.white.withOpacity(0.1),
+                              foregroundColor: Colors.white,
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.notifications_none,
+                                ),
+                                onPressed: () {},
+                              ),
+                            )
+                          ],
                         ),
-                        CircleAvatar(
-                          backgroundColor: Colors.white.withOpacity(0.1),
-                          foregroundColor: Colors.white,
-                          child: IconButton(
-                            icon: const Icon(
-                              Icons.notifications_none,
-                            ),
-                            onPressed: () {},
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 52,
-                    margin: EdgeInsets.only(left: 16),
-                    child: Row(
-                      children: [
-                        for (var i = 0; i < smsaTabMenuData.length; i++)
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _tabIndex = i;
-                              });
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: _tabIndex == i ? Colors.indigoAccent[700] : Colors.white.withOpacity(0.1),
-                                borderRadius: BorderRadius.circular(32),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 12,
-                              ),
-                              margin: const EdgeInsets.only(right: 12),
-                              child: Center(
-                                child: Text(
-                                  smsaTabMenuData[i].title ?? "??",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(16, 16, 0, 0),
-                      child: IndexedStack(
-                        index: _tabIndex,
-                        children: [
-                          SingleChildScrollView(
-                            child: Column(
-                              children: [
-                                SizedBox(height: 16,),
-                                Container(
-                                  height: 200,
-                                  color: Colors.blue,
-                                ),
-                                SizedBox(height: 16,),
-                                Text("Artis"),
-                                SizedBox(height: 16,),
-                                Container(
-                                  height: 100,
-                                  color: Colors.blue,
-                                ),
-                                SizedBox(height: 16,),
-                                Text("Popular today"),
-                                Container(
-                                  height: 200,
-                                  color: Colors.blue,
-                                ),
-                              ],
-
-                            ),
-                          ),
-                          Center(
-                            child: Text(_tabIndex.toString()),
-                          ),
-                          Center(
-                            child: Text(_tabIndex.toString()),
-                          ),
-                          Center(
-                            child: Text(_tabIndex.toString()),
-                          )
-                          // for (var i = 0; i < smsaTabMenuData.length; i++)
-                          //   Container(
-                          //     child: Center(
-                          //       child: Text(
-                          //         "${i}",
-                          //         style: TextStyle(
-                          //           color: Colors.white,
-                          //         ),
-                          //       ),
-                          //     ),
-                          //   ),
-                        ],
                       ),
+                      Container(
+                        height: 52,
+                        margin: EdgeInsets.only(left: 16),
+                        child: Row(
+                          children: [
+                            for (var i = 0; i < smsaTabMenuData.length; i++)
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _tabIndex = i;
+                                  });
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: _tabIndex == i ? Colors.indigoAccent[700] : Colors.white.withOpacity(0.1),
+                                    borderRadius: BorderRadius.circular(32),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 12,
+                                  ),
+                                  margin: const EdgeInsets.only(right: 12),
+                                  child: Center(
+                                    child: Text(
+                                      smsaTabMenuData[i].title ?? "??",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(16, 16, 0, 0),
+                          child: IndexedStack(
+                            index: _tabIndex,
+                            children: [
+                              SingleChildScrollView(
+                                child: Column(
+                                  children: [
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Container(
+                                      height: 200,
+                                      color: Colors.blue,
+                                    ),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Text("Artis"),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Container(
+                                      height: 100,
+                                      color: Colors.blue,
+                                    ),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Text("Popular today"),
+                                    Container(
+                                      height: 200,
+                                      color: Colors.blue,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Center(
+                                child: Text(_tabIndex.toString()),
+                              ),
+                              Center(
+                                child: Text(_tabIndex.toString()),
+                              ),
+                              Center(
+                                child: Text(_tabIndex.toString()),
+                              )
+                              // for (var i = 0; i < smsaTabMenuData.length; i++)
+                              //   Container(
+                              //     child: Center(
+                              //       child: Text(
+                              //         "${i}",
+                              //         style: TextStyle(
+                              //           color: Colors.white,
+                              //         ),
+                              //       ),
+                              //     ),
+                              //   ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: 16,
+                  left: 16,
+                  bottom: 4,
+                  child: Container(
+                    height: 64,
+                    decoration: BoxDecoration(
+                      color: Colors.indigoAccent,
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                ],
-              ),
+                )
+              ],
             ),
-            Positioned(
-              right: 16,
-              left: 16,
-              bottom: 4,
-              child: Container(
-                height: 64,
-                decoration: BoxDecoration(
-                  color: Colors.indigoAccent,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
+            Center(
+              child: Text("${_menuIndex}"),
+            ),
+            Center(
+              child: Text("${_menuIndex}"),
+            ),
+            Center(
+              child: Text("${_menuIndex}"),
             )
           ],
         ),
