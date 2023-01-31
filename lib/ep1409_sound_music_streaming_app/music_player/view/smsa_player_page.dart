109ac81ef2f550119d42959630c02d80c441406b
Dreamwalker <aristojeff@gmail.com>
2023-01-31 16:56:44 +0900

    Create smsa_player_page.dart

---
+++ lib/ep1409_sound_music_streaming_app/music_player/view/smsa_player_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SmsaPlayerPage extends StatefulWidget {
+  const SmsaPlayerPage({Key? key}) : super(key: key);
+
+  @override
+  State<SmsaPlayerPage> createState() => _SmsaPlayerPageState();
+}
+
+class _SmsaPlayerPageState extends State<SmsaPlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
