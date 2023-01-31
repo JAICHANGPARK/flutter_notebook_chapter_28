c33616f92e4c2ae050541e1cc305bfae1fccf53f
Dreamwalker <aristojeff@gmail.com>
2023-01-31 17:12:17 +0900

    Update smsa_player_page.dart

--- lib/ep1409_sound_music_streaming_app/music_player/view/smsa_player_page.dart
+++ lib/ep1409_sound_music_streaming_app/music_player/view/smsa_player_page.dart
@@ -63,7 +63,12 @@ class _SmsaPlayerPageState extends State<SmsaPlayerPage> {
               ),
             ),
             Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            )
           ],
         ),
       ),
