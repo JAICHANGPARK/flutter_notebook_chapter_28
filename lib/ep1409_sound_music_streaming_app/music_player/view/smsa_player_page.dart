3d9aeea57b842a2e2167fc6b17f836102fbd0360
Dreamwalker <aristojeff@gmail.com>
2023-01-31 17:05:34 +0900

    Update smsa_player_page.dart

--- lib/ep1409_sound_music_streaming_app/music_player/view/smsa_player_page.dart
+++ lib/ep1409_sound_music_streaming_app/music_player/view/smsa_player_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1409_sound_music_streaming_app/home/domain/smsa_menu_data.dart';
 
 class SmsaPlayerPage extends StatefulWidget {
   const SmsaPlayerPage({Key? key}) : super(key: key);
@@ -8,10 +9,15 @@ class SmsaPlayerPage extends StatefulWidget {
 }
 
 class _SmsaPlayerPageState extends State<SmsaPlayerPage> {
+  int _menuIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
         leading: IconButton(
           icon: const Icon(
             Icons.notifications_none,
@@ -26,6 +32,30 @@ class _SmsaPlayerPageState extends State<SmsaPlayerPage> {
             ),
           ),
         ],
+        title: Text("Title"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: SizedBox(
+          height: 72,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              for (var i = 0; i < smsaMenuData.length; i++)
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _menuIndex = i;
+                    });
+                  },
+                  icon: Icon(smsaMenuData[i].iconData),
+                  iconSize: 30,
+                  color: _menuIndex == i ? Colors.indigoAccent[700] : Colors.grey,
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
