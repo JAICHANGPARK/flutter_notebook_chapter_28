f39be7d8c0bb05fd256f9e9bd2697a5772ff04c6
Dreamwalker <aristojeff@gmail.com>
2023-02-01 18:29:31 +0900

    Update food_treat_start_page.dart

---
+++ lib/ep1414_food_treat/get_started/view/food_treat_start_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodTreatStartPage extends StatefulWidget {
+  const FoodTreatStartPage({Key? key}) : super(key: key);
+
+  @override
+  State<FoodTreatStartPage> createState() => _FoodTreatStartPageState();
+}
+
+class _FoodTreatStartPageState extends State<FoodTreatStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
