117ead29f58b856f4cd1f83beb400138f53065c2
Dreamwalker <aristojeff@gmail.com>
2023-02-01 18:39:25 +0900

    Update food_treat_start_page.dart

--- lib/ep1414_food_treat/get_started/view/food_treat_start_page.dart
+++ lib/ep1414_food_treat/get_started/view/food_treat_start_page.dart
@@ -12,42 +12,70 @@ class _FoodTreatStartPageState extends State<FoodTreatStartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text("Hello from FoodTread!"),
-            Text("What brings you to us?"),
-            Container(
-              height: 280,
-              color: Colors.pink,
-            ),
-            Container(
-              child: Row(
-                children: [
-                  Radio(
-                    value: 1,
-                    groupValue: 1,
-                    onChanged: (v) {},
-                  )
-                ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Hello from FoodTread!"),
+              Text("What brings you to us?"),
+              Container(
+                height: 300,
+                color: Colors.pink,
               ),
-            ),
-            Container(
-              child: Row(
-                children: [
-                  Radio(
-                    value: 1,
-                    groupValue: 1,
-                    onChanged: (v) {},
+              Container(
+                child: Row(
+                  children: [
+                    Radio(
+                      value: 1,
+                      groupValue: 1,
+                      onChanged: (v) {},
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Treatment"),
+                        Text("I would like to treat specific aliment"),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Radio(
+                      value: 1,
+                      groupValue: 1,
+                      onChanged: (v) {},
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Healthy lifestyle"),
+                        Text("I want to improve health & lifestyle"),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+                padding: EdgeInsets.symmetric(vertical: 24),
+                child: Center(
+                  child: Text(
+                    "Get started",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  Column(
-                    children: [
-                      Text("Healthy lifestyle"),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
