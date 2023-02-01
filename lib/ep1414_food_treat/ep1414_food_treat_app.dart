6d52082f4fed36f3c9efece5be4b212a441c2282
Dreamwalker <aristojeff@gmail.com>
2023-02-01 18:27:13 +0900

    Update ep1414_food_treat_app.dart

--- lib/ep1414_food_treat/ep1414_food_treat_app.dart
+++ lib/ep1414_food_treat/ep1414_food_treat_app.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
 
 
+void main() {
+  runApp(FoodTreatApp());
+}
 
+class FoodTreatApp extends StatelessWidget {
+  const FoodTreatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp()
+  }
+}
