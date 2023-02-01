a23adab4289824f01e044f0324bf6e077393251f
Dreamwalker <aristojeff@gmail.com>
2023-02-01 18:34:25 +0900

    Update food_treat_start_page.dart

--- lib/ep1414_food_treat/get_started/view/food_treat_start_page.dart
+++ lib/ep1414_food_treat/get_started/view/food_treat_start_page.dart
@@ -10,6 +10,30 @@ class FoodTreatStartPage extends StatefulWidget {
 class _FoodTreatStartPageState extends State<FoodTreatStartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("Hello from FoodTread!"),
+            Text("What brings you to us?"),
+            Container(
+              height: 280,
+              color: Colors.pink,
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Radio(
+                    value: 1,
+                    groupValue: 1,
+                    onChanged: (v) {},
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
