2f9b6edb04fa7b7ca8dc04f5c809fed01b2ba9ad
Dreamwalker <aristojeff@gmail.com>
2023-02-02 17:08:28 +0900

    Update food_treat_backgroud_widget.dart

--- lib/ep1414_food_treat/get_started/widgets/food_treat_backgroud_widget.dart
+++ lib/ep1414_food_treat/get_started/widgets/food_treat_backgroud_widget.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class FootTreatBackgroundWidget extends StatelessWidget {
+  const FootTreatBackgroundWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(247, 178, 142, 1),
+              Color.fromRGBO(248, 190, 167, 1),
+              Color.fromRGBO(250, 204, 192, 1),
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+      ),
+    );
+  }
+}
