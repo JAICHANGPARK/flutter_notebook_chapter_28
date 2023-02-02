0bab92eea9c63672bc9818c10addf4a00ceea00e
Dreamwalker <aristojeff@gmail.com>
2023-02-02 17:10:37 +0900

    Update foottreat_treatment_page.dart

--- lib/ep1414_food_treat/treatment/view/foottreat_treatment_page.dart
+++ lib/ep1414_food_treat/treatment/view/foottreat_treatment_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_28/ep1414_food_treat/get_started/widgets/food_treat_backgroud_widget.dart';
 
-
 class FoodTreadTreatmentPage extends StatefulWidget {
   const FoodTreadTreatmentPage({Key? key}) : super(key: key);
 
@@ -12,18 +11,24 @@ class FoodTreadTreatmentPage extends StatefulWidget {
 class _FoodTreadTreatmentPageState extends State<FoodTreadTreatmentPage> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        FootTreatBackgroundWidget(),
-        Positioned.fill(
-          child: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text("Treatment"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          FootTreatBackgroundWidget(),
+          SafeArea(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+                    Expanded(child: Text("Treatment")),
+                  ],
+                )
+              ],
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
