8d5210d39860b7d31edb98a2a8303dea2bc6403c
Dreamwalker <aristojeff@gmail.com>
2023-02-03 17:10:44 +0900

    Update ep1416_main.dart

--- lib/ep1416_cafe_finder_app/ep1416_main.dart
+++ lib/ep1416_cafe_finder_app/ep1416_main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
-
+void main() {
+  runApp(CafeFinderApp());
+}
 
 class CafeFinderApp extends StatelessWidget {
   const CafeFinderApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
