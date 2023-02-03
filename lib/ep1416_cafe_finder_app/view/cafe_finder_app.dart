fb2f4fcfd1477f981f4ad35a31182da2e5312e82
Dreamwalker <aristojeff@gmail.com>
2023-02-03 17:17:22 +0900

    Update cafe_finder_app.dart

--- lib/ep1416_cafe_finder_app/view/cafe_finder_app.dart
+++ lib/ep1416_cafe_finder_app/view/cafe_finder_app.dart
@@ -8,10 +8,12 @@ class CafeFinderMainPage extends StatefulWidget {
 }
 
 class _CafeFinderMainPageState extends State<CafeFinderMainPage> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(244, 246, 248, 1),
+      backgroundColor: const Color.fromRGBO(244, 246, 248, 1),
       bottomNavigationBar: BottomAppBar(
         color: Colors.white,
         height: 80,
@@ -20,21 +22,46 @@ class _CafeFinderMainPageState extends State<CafeFinderMainPage> {
             Column(
               children: [
                 IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.home_filled),
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 0;
+                    });
+                  },
+                  icon: const Icon(Icons.home_filled),
                   iconSize: 26,
                 ),
-                CircleAvatar(
-                  radius: 4,
-                  backgroundColor: Colors.black,
-                )
+                selectedIndex == 0
+                    ? const CircleAvatar(
+                        radius: 4,
+                        backgroundColor: Colors.black,
+                      )
+                    : Container(),
+              ],
+            ),
+            Column(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 1;
+                    });
+                  },
+                  icon: const Icon(Icons.favorite_border),
+                  iconSize: 26,
+                ),
+                selectedIndex == 1
+                    ? const CircleAvatar(
+                        radius: 4,
+                        backgroundColor: Colors.black,
+                      )
+                    : Container(),
               ],
             ),
             Column(
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.favorite_border),
+                  icon: const Icon(Icons.chat_outlined),
                   iconSize: 26,
                 ),
                 // CircleAvatar(
@@ -47,20 +74,7 @@ class _CafeFinderMainPageState extends State<CafeFinderMainPage> {
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.chat_outlined),
-                  iconSize: 26,
-                ),
-                // CircleAvatar(
-                //   radius: 4,
-                //   backgroundColor: Colors.black,
-                // )
-              ],
-            ),
-            Column(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
+                  icon: const Icon(
                     Icons.person_outline,
                   ),
                   iconSize: 26,
