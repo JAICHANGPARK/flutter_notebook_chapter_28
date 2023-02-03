20f6161e0efb089d70f16bcb111232fad932a70e
Dreamwalker <aristojeff@gmail.com>
2023-02-03 17:12:09 +0900

    Create cafe_finder_app.dart

---
+++ lib/ep1416_cafe_finder_app/view/cafe_finder_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CafeFinderApp extends StatefulWidget {
+  const CafeFinderApp({Key? key}) : super(key: key);
+
+  @override
+  State<CafeFinderApp> createState() => _CafeFinderAppState();
+}
+
+class _CafeFinderAppState extends State<CafeFinderApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
