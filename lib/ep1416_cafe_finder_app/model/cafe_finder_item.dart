bf5678f6c2d06e04470969cb7a4addd0280ea34c
Dreamwalker <aristojeff@gmail.com>
2023-02-05 09:51:13 +0900

    Update cafe_finder_item.dart

--- lib/ep1416_cafe_finder_app/model/cafe_finder_item.dart
+++ lib/ep1416_cafe_finder_app/model/cafe_finder_item.dart
@@ -1,5 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'cafe_finder_item.freezed.dart';
+
 @freezed
 class CafeFinderItem with _$CafeFinderItem {
   factory CafeFinderItem({
