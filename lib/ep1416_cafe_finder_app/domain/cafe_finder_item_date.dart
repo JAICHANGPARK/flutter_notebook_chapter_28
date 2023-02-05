bb73e241f465b1f60e736a219359e97847ad18fb
Dreamwalker <aristojeff@gmail.com>
2023-02-05 09:56:25 +0900

    Update cafe_finder_item_date.dart

--- lib/ep1416_cafe_finder_app/domain/cafe_finder_item_date.dart
+++ lib/ep1416_cafe_finder_app/domain/cafe_finder_item_date.dart
@@ -2,11 +2,21 @@ import 'package:flutter_notebook_chapter_28/ep1416_cafe_finder_app/model/cafe_fi
 
 List<CafeFinderItem> cafeFinderItems = [
   CafeFinderItem(
-    isSale: false,
+    isSale: true,
     title: "Title & Title Kitchen",
     subtitle: "Coffee, Western, Local",
     rate: 4.8,
     distance: 1.2,
     img: "https://cdn.pixabay.com/photo/2016/11/29/12/54/cafe-1869656_960_720.jpg",
+    review: "Rp. 15k - 35k"
+  ),
+  CafeFinderItem(
+      isSale: false,
+      title: "Title & Title Kitchen",
+      subtitle: "Coffee, Western, Local",
+      rate: 4.8,
+      distance: 1.2,
+      img: "https://cdn.pixabay.com/photo/2016/11/29/12/54/cafe-1869656_960_720.jpg",
+      review: "Rp. 15k - 35k"
   )
 ];
