7d30b350eb9ec9370914de975adc12841b9a4e2f
Dreamwalker <aristojeff@gmail.com>
2023-02-05 09:54:55 +0900

    Update cafe_finder_item_date.dart

--- lib/ep1416_cafe_finder_app/domain/cafe_finder_item_date.dart
+++ lib/ep1416_cafe_finder_app/domain/cafe_finder_item_date.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_notebook_chapter_28/ep1416_cafe_finder_app/model/cafe_finder_item.dart';
 
 List<CafeFinderItem> cafeFinderItems = [
-  CafeFinderItem()
+  CafeFinderItem(
+    isSale: false,
+    title: "Title & Title Kitchen",
+    subtitle: "Coffee, Western, Local",
+    rate: 4.8
+  )
 ];
