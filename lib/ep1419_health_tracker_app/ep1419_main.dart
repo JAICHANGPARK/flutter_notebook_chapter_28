3bd636931388fdab27fbffeb7ce0c7bf22247e5c
Dreamwalker <aristojeff@gmail.com>
2023-02-06 16:41:28 +0900

    Update ep1419_main.dart

---
+++ lib/ep1419_health_tracker_app/ep1419_main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class HealthTrackerApp extends StatelessWidget {
+  const HealthTrackerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
