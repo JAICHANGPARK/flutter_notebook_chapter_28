08d39899a23ab48e937cfb8ae9d2761ff9dd42f9
Dreamwalker <aristojeff@gmail.com>
2023-02-06 16:57:01 +0900

    Update health_check_home_screen.dart

--- lib/ep1419_health_tracker_app/home/view/health_check_home_screen.dart
+++ lib/ep1419_health_tracker_app/home/view/health_check_home_screen.dart
@@ -12,28 +12,55 @@ class _HealthCheckHomeScreenState extends State<HealthCheckHomeScreen> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            Container(
-              height: 40,
-              width: 40,
-              decoration: BoxDecoration(
-                color: Colors.orange,
+        Container(
+          padding: EdgeInsets.all(16),
+          color: Colors.white,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text("Hello👋,"),
+                      Text("Dreamwalker"),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications_none,
+                    ),
+                  )
+                ],
               ),
-            ),
-            Column(
-              children: [
-                Text("Hello👋,"),
-                Text("Dreamwalker"),
-              ],
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.notifications_none,
+              Row(
+                children: [
+                  Column(
+                    children: [Text("1,032"), Text("Daily Avg Carbs")],
+                  ),
+                  Text("Goal : 312 gr"),
+                ],
               ),
-            )
-          ],
+              Container(
+                height: 300,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
+        Container(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [],
+            ),
+          ),
         )
       ],
     );
