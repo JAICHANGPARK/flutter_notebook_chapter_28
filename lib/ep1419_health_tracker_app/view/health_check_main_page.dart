ec10172c863ff1afb350c71fc7be1940b5c237a0
Dreamwalker <aristojeff@gmail.com>
2023-02-06 16:44:49 +0900

    Update health_check_main_page.dart

---
+++ lib/ep1419_health_tracker_app/view/health_check_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HealthCheckMainPage extends StatefulWidget {
+  const HealthCheckMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<HealthCheckMainPage> createState() => _HealthCheckMainPageState();
+}
+
+class _HealthCheckMainPageState extends State<HealthCheckMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
