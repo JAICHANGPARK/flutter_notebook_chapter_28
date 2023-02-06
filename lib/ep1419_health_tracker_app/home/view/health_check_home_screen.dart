54fe7c756b978c22c988b0866f5c2520e3d53bed
Dreamwalker <aristojeff@gmail.com>
2023-02-06 16:44:53 +0900

    Update health_check_home_screen.dart

---
+++ lib/ep1419_health_tracker_app/home/view/health_check_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HealthCheckHomeScreen extends StatefulWidget {
+  const HealthCheckHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HealthCheckHomeScreen> createState() => _HealthCheckHomeScreenState();
+}
+
+class _HealthCheckHomeScreenState extends State<HealthCheckHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
