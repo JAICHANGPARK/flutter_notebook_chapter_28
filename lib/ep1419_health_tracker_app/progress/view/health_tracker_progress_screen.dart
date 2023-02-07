1d130685c3d12a1338974e50a3bd265c3fd290cd
Dreamwalker <aristojeff@gmail.com>
2023-02-07 19:23:04 +0900

    Update health_tracker_progress_screen.dart

--- lib/ep1419_health_tracker_app/progress/view/health_tracker_progress_screen.dart
+++ lib/ep1419_health_tracker_app/progress/view/health_tracker_progress_screen.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
 
+class HealthTrackerProgressScreen extends StatefulWidget {
+  const HealthTrackerProgressScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HealthTrackerProgressScreen> createState() => _HealthTrackerProgressScreenState();
+}
+
+class _HealthTrackerProgressScreenState extends State<HealthTrackerProgressScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Progress",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
+        )
+      ],
+    );
+  }
+}
