3cd95e38c6d2cbf5fd1d7bfe603e7a3f774d783d
Dreamwalker <aristojeff@gmail.com>
2023-02-09 23:19:22 +0900

    Update health_tracker_progress_screen.dart

--- lib/ep1419_health_tracker_app/progress/view/health_tracker_progress_screen.dart
+++ lib/ep1419_health_tracker_app/progress/view/health_tracker_progress_screen.dart
@@ -11,7 +11,7 @@ class _HealthTrackerProgressScreenState extends State<HealthTrackerProgressScree
   List<DateTime> calendarItems = List.generate(
     7,
     (index) => DateTime.now().subtract(
-      Duration(days: 7 - index),
+      Duration(days: 6 - index),
     ),
   );
   TextStyle calTextWeekdayStyle = const TextStyle(
@@ -19,6 +19,7 @@ class _HealthTrackerProgressScreenState extends State<HealthTrackerProgressScree
   );
   TextStyle calTextDayStyle = const TextStyle(
     fontWeight: FontWeight.bold,
+    fontSize: 16,
   );
 
   Widget buildDayTextWidget(int week) {
@@ -71,32 +72,44 @@ class _HealthTrackerProgressScreenState extends State<HealthTrackerProgressScree
         Container(
           color: Colors.white,
           padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
-          child: const Text(
-            "Progress",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 34,
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Progress",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 34,
+                ),
+              ),
+              Container(
+                height: 64,
+                color: Colors.pink,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: calendarItems.length,
+                  itemBuilder: (context, index) {
+                    final item = calendarItems[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey[50],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          buildDayTextWidget(item.weekday),
+                          Text("${item.day}"),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
           ),
         ),
-        Container(
-          height: 64,
-          color: Colors.pink,
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              final item = calendarItems[index];
-              return Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-                child: Column(
-                  children: [
-                    buildDayTextWidget(item.weekday),
-                    Text("${item.day}"),
-                  ],
-                ),
-              );
-            },
-          ),
-        )
       ],
     );
   }
