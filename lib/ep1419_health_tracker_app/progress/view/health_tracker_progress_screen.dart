c01a75e309e483b8d0eadd1bf1253fe9413ca197
Dreamwalker <aristojeff@gmail.com>
2023-02-09 23:11:47 +0900

    Update health_tracker_progress_screen.dart

--- lib/ep1419_health_tracker_app/progress/view/health_tracker_progress_screen.dart
+++ lib/ep1419_health_tracker_app/progress/view/health_tracker_progress_screen.dart
@@ -8,16 +8,34 @@ class HealthTrackerProgressScreen extends StatefulWidget {
 }
 
 class _HealthTrackerProgressScreenState extends State<HealthTrackerProgressScreen> {
+  List<DateTime> calendarItems = List.generate(
+    7,
+    (index) => DateTime.now().subtract(
+      Duration(days: 7 - index),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Text(
-          "Progress",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 34,
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.fromLTRB(16, 24, 16, 16),
+          child: Text(
+            "Progress",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 34,
+            ),
           ),
+        ),
+        Container(
+          height: 64,
+          color: Colors.pink,
+          child: ListView.builder(itemBuilder: (context, index) {
+            return Container();
+          }),
         )
       ],
     );
