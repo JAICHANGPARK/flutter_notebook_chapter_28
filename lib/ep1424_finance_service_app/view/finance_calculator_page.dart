9d6fb07b1630cd0914d831c2ea561cc9a897e682
Dreamwalker <aristojeff@gmail.com>
2023-02-11 11:04:48 +0900

    Update finance_calculator_page.dart

--- lib/ep1424_finance_service_app/view/finance_calculator_page.dart
+++ lib/ep1424_finance_service_app/view/finance_calculator_page.dart
@@ -13,101 +13,111 @@ class _FinanceCalculatorPageState extends State<FinanceCalculatorPage> {
     return Scaffold(
       backgroundColor: Colors.brown[100],
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 24, 16, 24),
-              child: Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 24,
-                  ),
-                  CircleAvatar(
-                    radius: 24,
-                    backgroundColor: Colors.brown[300],
-                    foregroundColor: Colors.white,
-                    child: const Icon(Icons.add),
-                  ),
-                  Spacer(),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(24),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 24, 0, 24),
+                child: Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 24,
                     ),
-                    padding: const EdgeInsets.only(left: 2, bottom: 2, top: 2, right: 8),
-                    child: const Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 22,
-                          backgroundColor: Colors.black,
-                          foregroundColor: Colors.white,
-                          child: Icon(
-                            Icons.import_export,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Text("Transfer"),
-                      ],
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Colors.brown[300],
+                      foregroundColor: Colors.white,
+                      child: const Icon(Icons.add),
                     ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 24),
-              height: 72,
-              width: 64,
-              color: Colors.orange,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 4,
-                    right: 4,
-                    top: 0,
-                    bottom: 16,
-                    child: Container(
+                    Spacer(),
+                    Container(
                       decoration: BoxDecoration(
-                        color: Colors.blueAccent,
-                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24),
                       ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      height: 24,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "... 4433",
-                          style: TextStyle(
-                            color: Colors.white,
+                      padding: const EdgeInsets.only(left: 2, bottom: 2, top: 2, right: 8),
+                      child: const Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 22,
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            child: Icon(
+                              Icons.import_export,
+                            ),
                           ),
-                        ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("Transfer"),
+                        ],
                       ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 32),
-              child: Text(
-                "Dream Walker",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
+              Container(
+                margin: EdgeInsets.only(top: 24),
+                height: 72,
+                width: 64,
+                color: Colors.orange,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 4,
+                      right: 4,
+                      top: 0,
+                      bottom: 16,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 0,
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 24,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "... 4433",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 32),
+                child: Text(
+                  "Dream Walker",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 64,
+
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
