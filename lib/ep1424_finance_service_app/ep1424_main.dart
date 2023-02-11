7c10b2899b56a9cc639bd8a47ee3b1aa3da5386f
Dreamwalker <aristojeff@gmail.com>
2023-02-11 10:47:08 +0900

    Update ep1424_main.dart

--- lib/ep1424_finance_service_app/ep1424_main.dart
+++ lib/ep1424_finance_service_app/ep1424_main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
-
+void main() {
+  runApp(FinanceServiceApp());
+}
 
 class FinanceServiceApp extends StatelessWidget {
   const FinanceServiceApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
