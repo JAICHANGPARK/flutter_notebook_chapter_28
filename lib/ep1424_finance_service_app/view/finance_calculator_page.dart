38e05df8294119bb95f2e04cc9085e2d857a8860
Dreamwalker <aristojeff@gmail.com>
2023-02-11 10:51:14 +0900

    Update finance_calculator_page.dart

---
+++ lib/ep1424_finance_service_app/view/finance_calculator_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FinanceCalculatorPage extends StatefulWidget {
+  const FinanceCalculatorPage({Key? key}) : super(key: key);
+
+  @override
+  State<FinanceCalculatorPage> createState() => _FinanceCalculatorPageState();
+}
+
+class _FinanceCalculatorPageState extends State<FinanceCalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
