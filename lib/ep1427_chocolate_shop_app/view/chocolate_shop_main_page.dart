e3c9ff9f216761a8cab8604ae0633f84b2f7d03d
Dreamwalker <aristojeff@gmail.com>
2023-02-14 17:25:42 +0900

    Update chocolate_shop_main_page.dart

--- lib/ep1427_chocolate_shop_app/view/chocolate_shop_main_page.dart
+++ lib/ep1427_chocolate_shop_app/view/chocolate_shop_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ChocolateShopMainPage extends StatefulWidget {
   const ChocolateShopMainPage({Key? key}) : super(key: key);
 
@@ -12,7 +11,26 @@ class _ChocolateShopMainPageState extends State<ChocolateShopMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80,
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.cake_outlined),
+                  Text("Home"),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
