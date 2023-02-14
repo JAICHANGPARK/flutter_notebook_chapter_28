8a274b99977fa3ee1ff83cfbf7e9e266a31db736
Dreamwalker <aristojeff@gmail.com>
2023-02-14 17:22:34 +0900

    Update ep1427_main.dart

--- lib/ep1427_chocolate_shop_app/ep1427_main.dart
+++ lib/ep1427_chocolate_shop_app/ep1427_main.dart
@@ -1,1 +1,16 @@
-i
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChocolateShotApp());
+}
+
+class ChocolateShotApp extends StatelessWidget {
+  const ChocolateShotApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
