257b5acb155e5785272111e49ef4ed9d565b782a
Dreamwalker <aristojeff@gmail.com>
2023-02-14 17:23:56 +0900

    Create chocolate_shop_main_page.dart

---
+++ lib/ep1427_chocolate_shop_app/view/chocolate_shop_main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class ChocolateShopMainPage extends StatefulWidget {
+  const ChocolateShopMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChocolateShopMainPage> createState() => _ChocolateShopMainPageState();
+}
+
+class _ChocolateShopMainPageState extends State<ChocolateShopMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
