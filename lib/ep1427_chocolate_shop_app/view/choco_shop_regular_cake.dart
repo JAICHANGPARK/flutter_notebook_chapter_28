ee7e22d0a88b4ba532aef34793de8ed5569f2c0f
Dreamwalker <aristojeff@gmail.com>
2023-02-17 16:16:47 +0900

    Create choco_shop_regular_cake.dart

---
+++ lib/ep1427_chocolate_shop_app/view/choco_shop_regular_cake.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChocoShopRegularCakePage extends StatefulWidget {
+  const ChocoShopRegularCakePage({Key? key}) : super(key: key);
+
+  @override
+  State<ChocoShopRegularCakePage> createState() => _ChocoShopRegularCakePageState();
+}
+
+class _ChocoShopRegularCakePageState extends State<ChocoShopRegularCakePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
