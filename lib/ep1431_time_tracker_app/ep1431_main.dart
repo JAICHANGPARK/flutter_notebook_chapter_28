1e7ab04072dd97c3d2db4dc9a13ee974add01342
Dreamwalker <aristojeff@gmail.com>
2023-02-18 11:19:50 +0900

    Update ep1431_main.dart

--- lib/ep1431_time_tracker_app/ep1431_main.dart
+++ lib/ep1431_time_tracker_app/ep1431_main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1431_time_tracker_app/view/time_tracker_home_page.dart';
 
 void main() {
-  runApp(TimeTrackerApp());
+  runApp(const TimeTrackerApp());
 }
 
 class TimeTrackerApp extends StatelessWidget {
@@ -9,6 +10,8 @@ class TimeTrackerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: TimeTrackerHomePage(),
+    );
   }
 }
