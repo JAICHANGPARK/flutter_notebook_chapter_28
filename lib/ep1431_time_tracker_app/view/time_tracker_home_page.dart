34b12277d2b67584ef2aa5fe33dae5cdf927d493
Dreamwalker <aristojeff@gmail.com>
2023-02-18 11:18:13 +0900

    Create time_tracker_home_page.dart

---
+++ lib/ep1431_time_tracker_app/view/time_tracker_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimeTrackerHomePage extends StatefulWidget {
+  const TimeTrackerHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TimeTrackerHomePage> createState() => _TimeTrackerHomePageState();
+}
+
+class _TimeTrackerHomePageState extends State<TimeTrackerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
