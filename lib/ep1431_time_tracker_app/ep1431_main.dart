7c2f2284a65364afa9dc12b70b2a43a933a3242b
Dreamwalker <aristojeff@gmail.com>
2023-02-18 11:16:56 +0900

    Create ep1431_main.dart

---
+++ lib/ep1431_time_tracker_app/ep1431_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TimeTrackerApp());
+}
+
+class TimeTrackerApp extends StatelessWidget {
+  const TimeTrackerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
