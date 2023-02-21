fc22d5e6b4abce56b5f12a7af25c1e6c0777c583
JAICHANGPARK <aristojeff@gmail.com>
2023-02-21 16:28:55 +0900

    Update ep1434_main.dart

--- lib/ep1434_freelancer_project_app/ep1434_main.dart
+++ lib/ep1434_freelancer_project_app/ep1434_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1434_freelancer_project_app/view/freelancer_main_page.dart';
 
 void main() {
   runApp(FreelancerProjectApp());
@@ -9,6 +10,10 @@ class FreelancerProjectApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        "/": (context) => FreelancerMainPage(),
+      },
+    );
   }
 }
