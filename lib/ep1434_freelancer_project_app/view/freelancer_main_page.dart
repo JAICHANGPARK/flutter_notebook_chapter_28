a5332de4e7bd28c1a6c96d6f5c686d6acfd9720c
JAICHANGPARK <aristojeff@gmail.com>
2023-02-21 16:27:45 +0900

    Update freelancer_main_page.dart

---
+++ lib/ep1434_freelancer_project_app/view/freelancer_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FreelancerMainPage extends StatefulWidget {
+  const FreelancerMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<FreelancerMainPage> createState() => _FreelancerMainPageState();
+}
+
+class _FreelancerMainPageState extends State<FreelancerMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
