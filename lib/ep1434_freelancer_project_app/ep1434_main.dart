5a984281d6505cd8b2c868a914c1d2fa654b130c
JAICHANGPARK <aristojeff@gmail.com>
2023-02-21 16:26:42 +0900

    Update ep1434_main.dart

---
+++ lib/ep1434_freelancer_project_app/ep1434_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FreelancerProjectApp());
+}
+
+class FreelancerProjectApp extends StatelessWidget {
+  const FreelancerProjectApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
