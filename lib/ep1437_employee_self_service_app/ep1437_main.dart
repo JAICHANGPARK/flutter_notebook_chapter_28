1945093ff1b4e8b98a5173886deead843f61e281
JAICHANGPARK <aristojeff@gmail.com>
2023-02-24 16:23:43 +0900

    Create ep1437_main.dart

---
+++ lib/ep1437_employee_self_service_app/ep1437_main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+
+
+}
+
+
+class EmployeeSelfServiceApp extends StatelessWidget {
+  const EmployeeSelfServiceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
