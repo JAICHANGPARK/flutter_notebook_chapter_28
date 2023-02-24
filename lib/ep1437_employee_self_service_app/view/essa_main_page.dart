10fa7eaa11fdaef2ec18ec39e4c99ddec84035c7
JAICHANGPARK <aristojeff@gmail.com>
2023-02-24 16:25:19 +0900

    Create essa_main_page.dart

---
+++ lib/ep1437_employee_self_service_app/view/essa_main_page.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+class ESSAMainPage extends StatefulWidget {
+  const ESSAMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<ESSAMainPage> createState() => _ESSAMainPageState();
+}
+
+class _ESSAMainPageState extends State<ESSAMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
