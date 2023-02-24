4d1abe80e3e8af576dcc4e39e257f9bdc8a01e46
JAICHANGPARK <aristojeff@gmail.com>
2023-02-24 16:24:06 +0900

    Update ep1437_main.dart

--- lib/ep1437_employee_self_service_app/ep1437_main.dart
+++ lib/ep1437_employee_self_service_app/ep1437_main.dart
@@ -1,17 +1,16 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-
-
+  runApp(const EmployeeSelfServiceApp());
 }
-
 
 class EmployeeSelfServiceApp extends StatelessWidget {
   const EmployeeSelfServiceApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      
+    );
   }
 }
