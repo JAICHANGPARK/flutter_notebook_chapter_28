d0f10ff74964145ccac1170837bb9e77bc6e8426
JAICHANGPARK <aristojeff@gmail.com>
2023-02-24 16:30:38 +0900

    Update essa_main_page.dart

--- lib/ep1437_employee_self_service_app/view/essa_main_page.dart
+++ lib/ep1437_employee_self_service_app/view/essa_main_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ESSAMainPage extends StatefulWidget {
@@ -13,10 +12,58 @@ class _ESSAMainPageState extends State<ESSAMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Expanded(child: Placeholder()),
+                          SizedBox(height: 8,),
+                          Expanded(child: Placeholder()),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 8,),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Expanded(child: Placeholder()),
+                          SizedBox(height: 8,),
+                          Expanded(child: Placeholder()),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Reduce the workloads\nof HR management."),
+                    Text("Help you to improve efficiency, accuracy, engagement, "
+                        "and cost savings for employers."),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("I'm a Manager"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("I'm a Employee"),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
