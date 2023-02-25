044ab58da3be61bb72756623a7d0a09c21cc1beb
JAICHANGPARK <aristojeff@gmail.com>
2023-02-26 08:41:45 +0900

    Update essa_home_page.dart

--- lib/ep1437_employee_self_service_app/view/essa_home_page.dart
+++ lib/ep1437_employee_self_service_app/view/essa_home_page.dart
@@ -9,13 +9,31 @@ class EssaHomePage extends StatefulWidget {
 }
 
 class _EssaHomePageState extends State<EssaHomePage> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [
-
+          children: const [],
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: BottomNavigationBar(
+          currentIndex: currentIndex,
+          type: BottomNavigationBarType.fixed,
+          onTap: (idx) {
+            setState(() {
+              currentIndex = idx;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications_none), label: "Notification"),
+            BottomNavigationBarItem(icon: Icon(Icons.access_time_rounded), label: "Time Off"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
           ],
         ),
       ),
