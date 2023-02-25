c06bb068004eaea64e8b5a070bd8ac2871b51989
JAICHANGPARK <aristojeff@gmail.com>
2023-02-26 08:37:08 +0900

    Create essa_home_page.dart

---
+++ lib/ep1437_employee_self_service_app/view/essa_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+///Essa = Employee Self Service App
+class EssaHomePage extends StatefulWidget {
+  const EssaHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<EssaHomePage> createState() => _EssaHomePageState();
+}
+
+class _EssaHomePageState extends State<EssaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
