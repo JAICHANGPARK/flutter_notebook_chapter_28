efdb74fc0c990d1c6de2a65fb1a1a0ff23bc177d
JAICHANGPARK <aristojeff@gmail.com>
2023-02-28 18:01:52 +0900

    Update ep1441_main.dart

---
+++ lib/ep1441_news_portal_app/ep1441_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NewsPortalApp());
+}
+
+class NewsPortalApp extends StatelessWidget {
+  const NewsPortalApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
