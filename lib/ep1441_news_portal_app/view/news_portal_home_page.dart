173b4a9e7e997ec3687ca5a8efa0f00c411bd46d
JAICHANGPARK <aristojeff@gmail.com>
2023-02-28 18:04:51 +0900

    Update news_portal_home_page.dart

--- lib/ep1441_news_portal_app/view/news_portal_home_page.dart
+++ lib/ep1441_news_portal_app/view/news_portal_home_page.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class NewsPortalHomePage extends StatefulWidget {
+  const NewsPortalHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<NewsPortalHomePage> createState() => _NewsPortalHomePageState();
+}
+
+class _NewsPortalHomePageState extends State<NewsPortalHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
