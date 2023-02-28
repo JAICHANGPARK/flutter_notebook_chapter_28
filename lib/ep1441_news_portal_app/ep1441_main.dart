c9a0c66645a611e4f36776497655d093a94cd9e7
JAICHANGPARK <aristojeff@gmail.com>
2023-02-28 18:05:02 +0900

    Update ep1441_main.dart

--- lib/ep1441_news_portal_app/ep1441_main.dart
+++ lib/ep1441_news_portal_app/ep1441_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_28/ep1441_news_portal_app/view/news_portal_home_page.dart';
 
 void main() {
   runApp(NewsPortalApp());
@@ -9,6 +10,8 @@ class NewsPortalApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: NewsPortalHomePage(),
+    );
   }
 }
