933a7b7ba189fb662ca1fa5cd9bde536011dba7b
JAICHANGPARK <aristojeff@gmail.com>
2023-03-04 08:26:18 +0900

    Create social_trip_main_page.dart

---
+++ lib/ep1445_social_trip_app/view/social_trip_main_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SocialTripMainPage extends StatefulWidget {
+  const SocialTripMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<SocialTripMainPage> createState() => _SocialTripMainPageState();
+}
+
+class _SocialTripMainPageState extends State<SocialTripMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+
+    );
+  }
+}
