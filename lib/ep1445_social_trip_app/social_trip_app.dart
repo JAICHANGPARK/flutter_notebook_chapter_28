2b5fb21d6919311e0d2849d48bfc5e8f7efefe2e
JAICHANGPARK <aristojeff@gmail.com>
2023-03-04 08:22:35 +0900

    Update social_trip_app.dart

---
+++ lib/ep1445_social_trip_app/social_trip_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SocialTripApp extends StatelessWidget {
+  const SocialTripApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
