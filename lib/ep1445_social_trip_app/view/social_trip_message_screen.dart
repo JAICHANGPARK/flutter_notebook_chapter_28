fdbee634b5e71cc30e391b495b24c6ef81513de5
JAICHANGPARK <aristojeff@gmail.com>
2023-03-06 16:49:29 +0900

    Update social_trip_message_screen.dart

--- lib/ep1445_social_trip_app/view/social_trip_message_screen.dart
+++ lib/ep1445_social_trip_app/view/social_trip_message_screen.dart
@@ -1,1 +1,20 @@
 
+import 'package:flutter/material.dart';
+
+class SocialTripMessageScreen extends StatefulWidget {
+  const SocialTripMessageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SocialTripMessageScreen> createState() => _SocialTripMessageScreenState();
+}
+
+class _SocialTripMessageScreenState extends State<SocialTripMessageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
