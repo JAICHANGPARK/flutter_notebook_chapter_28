ceb6954ba159efd6b96d524d890eb21bdbbc7357
JAICHANGPARK <aristojeff@gmail.com>
2023-03-06 16:46:59 +0900

    Create social_trip_screen.dart

---
+++ lib/ep1445_social_trip_app/view/social_trip_screen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class SocialTripScreen extends StatefulWidget {
+  const SocialTripScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SocialTripScreen> createState() => _SocialTripScreenState();
+}
+
+class _SocialTripScreenState extends State<SocialTripScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
