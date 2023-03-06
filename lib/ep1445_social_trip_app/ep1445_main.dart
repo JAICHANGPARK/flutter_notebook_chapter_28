29dfabce05bb01b9501d603b459ccaffdcb04f61
JAICHANGPARK <aristojeff@gmail.com>
2023-03-06 16:40:30 +0900

    Update ep1445_main.dart

--- lib/ep1445_social_trip_app/ep1445_main.dart
+++ lib/ep1445_social_trip_app/ep1445_main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_28/ep1445_social_trip_app/social_trip_app.dart';
 
 void main() {
-  runApp(SocialTripApp());
+  runApp(const SocialTripApp());
 }
