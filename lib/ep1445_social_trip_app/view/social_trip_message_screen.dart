b8ae6a4b03751961047ab3a47b86d7083f1298e4
JAICHANGPARK <aristojeff@gmail.com>
2023-03-06 17:00:18 +0900

    Update social_trip_message_screen.dart

--- lib/ep1445_social_trip_app/view/social_trip_message_screen.dart
+++ lib/ep1445_social_trip_app/view/social_trip_message_screen.dart
@@ -10,84 +10,96 @@ class SocialTripMessageScreen extends StatefulWidget {
 class _SocialTripMessageScreenState extends State<SocialTripMessageScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          "Message",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-        Container(
-          height: 48,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey[400]!,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
             ),
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: TextField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.search),
-              hintText: "Search",
+            child: Text(
+              "Message",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
             ),
           ),
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(6),
-                ),
-                child: Center(
-                  child: Text(
-                    "Direct",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+          Container(
+            height: 48,
+            margin: EdgeInsets.symmetric(vertical: 16),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey[400]!,
+              ),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.search),
+                hintText: "Search",
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Direct",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(width: 12),
-            Expanded(
-              child: Container(
-                child: Center(
-                  child: Text(
-                    "Connect",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+              SizedBox(width: 12),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: Center(
+                    child: Text(
+                      "Connect",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(width: 12),
-            Expanded(
-              child: Container(
-                child: Center(
-                  child: Text(
-                    "Requests",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+              SizedBox(width: 12),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: Center(
+                    child: Text(
+                      "Requests",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        Expanded(
-          child: ListView.separated(
-            itemBuilder: (context, index) {},
-            separatorBuilder: (context, _) => Divider(),
-            itemCount: 10,
+            ],
           ),
-        ),
-      ],
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (context, index) {},
+              separatorBuilder: (context, _) => Divider(),
+              itemCount: 10,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
