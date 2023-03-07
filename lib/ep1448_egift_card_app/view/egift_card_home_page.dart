aeb8221512d79a71a59644a8063176bcd89a1e90
JAICHANGPARK <aristojeff@gmail.com>
2023-03-07 19:30:37 +0900

    Create egift_card_home_page.dart

---
+++ lib/ep1448_egift_card_app/view/egift_card_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class EGiftCardHomePage extends StatefulWidget {
+  const EGiftCardHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<EGiftCardHomePage> createState() => _EGiftCardHomePageState();
+}
+
+class _EGiftCardHomePageState extends State<EGiftCardHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
