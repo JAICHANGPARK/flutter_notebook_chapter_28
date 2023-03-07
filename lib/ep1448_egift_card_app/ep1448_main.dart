3b09c483bf6e365943d0344f25df53f36942baa2
JAICHANGPARK <aristojeff@gmail.com>
2023-03-07 19:29:58 +0900

    Update ep1448_main.dart

---
+++ lib/ep1448_egift_card_app/ep1448_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EGiftCardApp());
+}
+
+class EGiftCardApp extends StatelessWidget {
+  const EGiftCardApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
